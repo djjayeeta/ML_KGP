fe80b7a33f27f9e60bcc5bef1c452ff05a24f646
Arundhati Banerjee <barundhati18@gmail.com>
2017-05-02 03:04:16 +0530

    To process the .tif image data

---
+++ process_tif.py
@@ -0,0 +1,35 @@
+from osgeo import gdal
+import pickle
+
+image_path = "./sundarbans_ISRO_data/IMS1_HYSI_GEO_114_05FEB2009_S1_TOA_REFLECTANCE_07_SPBIN.tif"
+img = gdal.Open(image_path,gdal.GA_ReadOnly)
+
+row_num = 272
+col_num = 292
+
+pixel = {}
+
+for i in range(1,18):
+	band = img.GetRasterBand(i).ReadAsArray()
+	for m in range(row_num):
+		pixel[m] = {}
+		for n in range(col_num):
+			#if i == 0:
+				pixel[m][n] = []
+			#	continue
+			#(pixel[m][n]).append(band[m][n])
+
+for i in range(1,18):
+	band = img.GetRasterBand(i).ReadAsArray()
+	for m in range(row_num):
+		#pixel[m] = {}
+		for n in range(col_num):
+			#if i == 0:
+			#	pixel[m][n] = []
+			#	continue
+			(pixel[m][n]).append(band[m][n]*0.001)
+
+f = open("process_tif.p","wb")
+pickle.dump(pixel, f)
+f.close()
+
