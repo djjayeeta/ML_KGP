7b4e97fb8eef046e236d5a89f5aa5e35f43cdc08
djjayeeta <dj.jayeeta@gmail.com>
2017-06-13 16:27:42 +0530

    weight done

--- pdhg_linear.py
+++ pdhg_linear.py
@@ -187,23 +187,23 @@ def pd_nonlocal_HSI(image,W,mu,endmem,lamda,tao,sigma,theta,tol,iter_stop,innerl
 	return error
 
 
-mat_file_path = "data_urban.mat"
-mat = scipy.io.loadmat(mat_file_path)
-f = mat['H']
-W = mat['W_binary_10']
-mu = 1e-5
-lamda = 1e6
-tao = 10.0
-sigma = 1.0/tao/4.0/10.0
-endmem_rand = mat['endmem_rand']
-sigma = sigma/3
-theta = 1
-tol = 1e-2
-iter_stop = 1.1
-innerloop = 5
-outerloop = 50
-output_path = "data_old/linear_hybrid_output_"
-endmem = endmem_rand
-image = f
-error = pd_nonlocal_HSI_stop_new(f,W,mu,endmem_rand,lamda,tao,sigma,theta,tol,iter_stop,innerloop,outerloop,output_path)
+# mat_file_path = "data_urban.mat"
+# mat = scipy.io.loadmat(mat_file_path)
+# f = mat['H']
+# W = mat['W_binary_10']
+# mu = 1e-5
+# lamda = 1e6
+# tao = 10.0
+# sigma = 1.0/tao/4.0/10.0
+# endmem_rand = mat['endmem_rand']
+# sigma = sigma/3
+# theta = 1
+# tol = 1e-2
+# iter_stop = 1.1
+# innerloop = 5
+# outerloop = 50
+# output_path = "data_old/linear_hybrid_output_"
+# endmem = endmem_rand
+# image = f
+# error = pd_nonlocal_HSI_stop_new(f,W,mu,endmem_rand,lamda,tao,sigma,theta,tol,iter_stop,innerloop,outerloop,output_path)
 	
